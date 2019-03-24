2d6a340917d02ee251a4b087b8e32faa99b43b77
Eloston <eloston@programmer.net>
2019-03-24 22:29:39 +0000

    build.sh: Fix path to flags.macos.gn

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ mkdir -p "$_download_cache"
 "$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
 "$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" -c "$_root_dir/build/domsubcache.tar.gz" "$_src_dir"
 cp "$_main_repo/flags.gn" "$_src_dir/out/Default/args.gn"
-cat "$_main_repo/flags.macos.gn" >> "$_src_dir/out/Default/args.gn"
+cat "$_root_dir/flags.macos.gn" >> "$_src_dir/out/Default/args.gn"
 
 cd "$_src_dir"
 
