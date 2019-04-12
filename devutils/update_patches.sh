786228fe586c6dc749302446212168e04cc95450
Saltama <44767529+saltama@users.noreply.github.com>
2019-04-12 13:48:59 +0200

    Fix for update_patches script

--- devutils/update_patches.sh
+++ devutils/update_patches.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
-PLATFORM_ROOT=$(dirname $(greadlink -f ${BASH_SOURCE[0]}))
-UNGOOGLED_REPO=$PLATFORM_ROOT/upstream/ungoogled-chromium
+PLATFORM_ROOT=$(dirname $(dirname $(greadlink -f ${BASH_SOURCE[0]})))
+UNGOOGLED_REPO=$PLATFORM_ROOT/ungoogled-chromium
 
 _command=$1
 
