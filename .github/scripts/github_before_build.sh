598bedc32e2297a58ad9f5ddeacd188bc53d0741
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-01-14 18:30:38 -0500

    ci: Share the same build resources across builds for both x86-64 and arm64
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- .github/scripts/github_before_build.sh
+++ .github/scripts/github_before_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-# Simple script for downloading, unpacking, and getting ready to build Ungoogled-Chromium macOS binaries on GitHub Actions
+# Simple script for getting ready to build Ungoogled-Chromium macOS binaries on GitHub Actions
 
 _target_cpu="${1:-x64}"
 
@@ -8,20 +8,6 @@ _root_dir=$(dirname $(greadlink -f $0))
 _download_cache="$_root_dir/build/download_cache"
 _src_dir="$_root_dir/build/src"
 _main_repo="$_root_dir/ungoogled-chromium"
-
-mkdir -p "$_src_dir"
-sudo df -h
-sudo du -hs "$_src_dir"
-
-rm -rf "$_src_dir/out" || true
-mkdir -p "$_src_dir/out/Default"
-mkdir -p "$_download_cache"
-
-"$_main_repo/utils/downloads.py" retrieve -i "$_main_repo/downloads.ini" "$_root_dir/downloads.ini" -c "$_download_cache"
-"$_main_repo/utils/downloads.py" unpack -i "$_main_repo/downloads.ini" "$_root_dir/downloads.ini" -c "$_download_cache" "$_src_dir"
-"$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
-"$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
-"$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" "$_src_dir"
 
 shopt -s nocasematch
 
