870e1ce5140251e84b6d172492bf3fd15e9ab4aa
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-27 13:27:20 -0400

    ci: Update Build Step
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- .github/scripts/github_fetch_resources.sh
+++ .github/scripts/github_fetch_resources.sh
@@ -15,10 +15,12 @@ rm -rf "$_src_dir/out" || true
 mkdir -p "$_src_dir/out/Default"
 mkdir -p "$_download_cache"
 
-"$_root_dir/retrieve_and_unpack_resource.sh"
+"$_root_dir/retrieve_and_unpack_resource.sh" -g
 
 "$_main_repo/utils/prune_binaries.py" "$_src_dir" "$_main_repo/pruning.list"
 "$_main_repo/utils/patches.py" apply "$_src_dir" "$_main_repo/patches" "$_root_dir/patches"
 "$_main_repo/utils/domain_substitution.py" apply -r "$_main_repo/domain_regex.list" -f "$_main_repo/domain_substitution.list" "$_src_dir"
+
+"$_root_dir/retrieve_and_unpack_resource.sh" -p
 
 rm -rvf "$_download_cache"
