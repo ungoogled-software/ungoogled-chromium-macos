c9c01b386a13cc2d9af8c16c87e8d4bab098feb4
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-08-23 21:40:43 -0400

    Add Announcement to Release when a Announcement File Exists
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- .github/scripts/github_generate_release_note.sh
+++ .github/scripts/github_generate_release_note.sh
@@ -13,10 +13,20 @@ _release_tag_version="${_chromium_version}-${_ungoogled_revision}.${_package_rev
 _gh_run_href="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
 touch ./github_release_note.md
-printf '## Ungoogled-Chromium macOS %s\n' "$_release_tag_version" | tee -a ./github_release_note.md
-printf '\n' | tee -a ./github_release_note.md
+printf '## Ungoogled-Chromium macOS %s\n\n' "$_release_tag_version" | tee -a ./github_release_note.md
+
+if [ -f $_root_dir/announcements.md ]; then
+    printf '### Announcements %s\n\n' | tee -a ./github_release_note.md
+    
+    _announcement="${_root_dir}/announcements.md"
+    cat $_announcement | tee -a ./github_release_note.md
+
+    printf '\n' | tee -a ./github_release_note.md
+    printf '### Release Assets Info %s\n\n' | tee -a ./github_release_note.md
+fi
+
 cat $_arm64_hash_name | tee -a ./github_release_note.md
 printf '\n' | tee -a ./github_release_note.md
 cat $_x64_hash_name | tee -a ./github_release_note.md
-printf '\n' | tee -a ./github_release_note.md
+printf '\n\n---\n\n' | tee -a ./github_release_note.md
 printf 'See [this GitHub Actions Run](%s) for the [Workflow file](%s/workflow) used as well as the build logs and artifacts\n' "$_gh_run_href" "$_gh_run_href" | tee -a ./github_release_note.md
