fb6f9b93cf1eef921d4192fde249a45c205f6421
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-01-21 08:37:37 -0500

    ci: Fix bindgen path issue
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- .github/scripts/github_build.sh
+++ .github/scripts/github_build.sh
@@ -16,6 +16,7 @@ _ninja_path="$_homebrew_path/ninja/bin"
 _python_path="$_homebrew_path/python3/bin"
 
 _root_dir=$(dirname $(greadlink -f $0))
+_src_dir="$_root_dir/build/src"
 if [[ -f "$_root_dir/epoch_job_start.txt" ]]; then
   epoch_job_start=$(cat "$_root_dir/epoch_job_start.txt")
   # GitHub's hard time limit is 6 h per job, we want to spare 1 h for steps before and after the build,
@@ -23,7 +24,9 @@ if [[ -f "$_root_dir/epoch_job_start.txt" ]]; then
   _remaining_time=$(( 360*60 - 60*60 - $(date +%s) + epoch_job_start ))
 fi
 
-cd build/src
+cd "$_src_dir"
+
+ln -s "$_src_dir/third_party" "$_src_dir/../third_party"
 
 echo $(date +%s) | tee -a "$_root_dir/build_times_$_target_cpu.log"
 echo "status=running" >> $GITHUB_OUTPUT
