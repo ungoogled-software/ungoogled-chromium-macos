2adaeccff6225d8cf338092c986d43e0ad534cef
jj <log@riseup.net>
2025-03-08 12:17:11 +0000

    Update to ungoogled-chromium 134.0.6998.35

--- retrieve_and_unpack_resource.sh
+++ retrieve_and_unpack_resource.sh
@@ -98,7 +98,7 @@ if $retrieve_arch_specific; then
     _rustc_dir="$_rust_dir/rustc"
     _rustc_lib_dir="$_rust_dir/rustc/lib/rustlib/$_rust_name/lib"
 
-    echo "rustc 1.85.0-nightly (9e136a30a 2024-12-19)" > "$_rust_flag_file"
+    echo "rustc 1.85.0-nightly (69fd5e405 2025-02-15)" > "$_rust_flag_file"
 
     mkdir -p "$_rust_bin_dir"
     mkdir -p "$_rust_dir/lib"
