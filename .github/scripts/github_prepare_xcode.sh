b3b3ae939b1184f0634582aad0652e23e4d17b8f
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-10-04 10:38:30 +0300

    Read symlinks when looking for Xcode

--- .github/scripts/github_prepare_xcode.sh
+++ .github/scripts/github_prepare_xcode.sh
@@ -3,7 +3,27 @@
 set -euo pipefail
 shopt -s extglob
 
-sudo rm -rf /Applications/Xcode_!(26).app
+ls -lrt /Applications
+
+# GitHub actions is in flux, and different versions of Xcode exist in different runs.
+BASE_XCODE_PATH=/Applications/Xcode_26.app
+if [ ! -e "$BASE_XCODE_PATH" ]; then
+  BASE_XCODE_PATH=/Applications/Xcode_26.0.app
+fi
+
+if [ ! -e "$BASE_XCODE_PATH" ]; then
+  echo "Failed to find a suitable version of Xcode"
+  exit 1
+fi
+
+TARGET_XCODE_VERSION="$(readlink -f "$BASE_XCODE_PATH" | xargs basename | sed 's/Xcode_//' | sed 's/.app//')"
+
+sudo rm -rf /Applications/Xcode_!("$TARGET_XCODE_VERSION").app
+if [ ! -e /Applications/Xcode_26.app ]; then
+  # try to keep paths consistent between jobs.
+  sudo mv "/Applications/Xcode_$TARGET_XCODE_VERSION.app" /Applications/Xcode_26.app
+fi
+
 sudo xcode-select --switch /Applications/Xcode_26.app
 sudo xcrun simctl delete all
 
