bdcb47e48c23c6c80821cd3e947a31173598266d
Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>
2026-08-22 22:46:45 -0400

    Fix Go-lang Toolchain
    Signed-off-by: Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>

--- .github/scripts/github_setup_env_toolchain.sh
+++ .github/scripts/github_setup_env_toolchain.sh
@@ -2,7 +2,7 @@
 
 # Simple script for setting up all toolchain dependencies for building Ungoogled-Chromium on macOS
 
-brew install ninja coreutils --overwrite
+brew install go ninja coreutils --overwrite
 
 # Install PySocks and httplib2 for Python from PyPI
 pip3 install PySocks httplib2 --break-system-packages
